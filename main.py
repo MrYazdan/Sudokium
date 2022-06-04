5c6fff866872ca532fd1ff95b8b1b6c44d799669
mryazdan <mrrezayazdani@yahoo.com>
2022-06-04 11:50:58 +0430

    Create todo roadmap

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from settings import configs
 
 def main():
+    # todo :
+    #   check user to install dependencies
+    #   get level from user
+    #   run browser driver
+    #   get sudoku table
+    #   solve sudoku by z3
+    #   set solved sudoku in web table
+    #   send sudoku
+    #   kill browser !
+
     pass
 
 if __name__ == "__main__":
